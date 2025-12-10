5d275f519c8bb1132d21f0fd6b7ce53d097793ac
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-12-10 16:36:07 +0100

    Create express app and register static assets

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.send('Film db')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+
+})
